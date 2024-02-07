d74814b442ae21b0854e65e0bf9ff7f96dad438b
Emma Larsson <emma940112@gmail.com>
2024-02-07 06:22:30 +0100

    Skapat en tabell utifrån ramschema_ht23.json

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*Moment 1*/
 //Variabler för huvudmeny
 let openBtnEl = document.getElementById("openmenu");
 let closeBtnEl = document.getElementById("closemenu");
@@ -19,4 +20,38 @@ function toggleMenu() {
     } else {
         navMenuEl.style.display = "none";
     }
+}
+
+/*Moment 2*/
+const url = ("../ramschema_ht23.json");
+window.onload = init();
+
+async function init() {
+    try {
+        //Fetch-anrop
+        const response = await fetch(url);
+        let courses = await response.json();
+
+        //Kör funktionen displayCourses
+        displayCourses(courses);
+
+    } catch {
+        document.getElementById("error").innerHTML = "<p>Något gick fel, prova igen senare.</p>"
+    }
+
+}
+
+function displayCourses(courses) {
+    const tableEl = document.getElementById("table");
+
+    //Loopa igenom och skriv ut till DOM
+    courses.forEach(course => {
+        tableEl.innerHTML += `
+        <tr>
+        <td>${course.code}</td>
+        <td>${course.coursename}</td>
+        <td>${course.progression}</td>
+        </tr>
+        `;
+    });
 }
